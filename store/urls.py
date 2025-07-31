e3ae51166a8382729dbd41f65f27cc2e4137b609
Anish oli <olianish534@gmail.com>
2025-07-31 14:55:05 +0545

    Add main template and navabar with cart and login button

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
+    path('',views.home,name = "home"),
     
-    path('',views.store,name="store"),
+    path('store',views.store,name="store"),
     path('cart/',views.cart,name="cart"),
     path('checkout/',views.checkout,name="checkout"),
 ]
--- store/views.py
+++ store/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+def home(request):
+    context={}
+    return render (request, 'store/Main.html',context)
+
 def store(request):
     context = {}
     return render(request, 'store/Store.html' ,context)
