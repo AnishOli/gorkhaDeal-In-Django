8aff19e2874786b9025870cacaaf5e22efe4938b
Anish oli <olianish534@gmail.com>
2025-07-31 10:04:03 +0545

    configure app, templates,views and urls

--- store/views.py
+++ store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def store(request):
+    context = {}
+    return render(request, 'store/Store.html' ,context)
+
+
+
+#view for cart
+
+def cart(request):
+    context = {}
+    return render(request, 'store/Cart.html' ,context)
+
+#view for checkout
+def checkout(request):
+    context = {}
+    return render(request, 'store/Checkout.html' ,context)
