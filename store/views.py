99315d76ff05aae385b114f993f5e438f3cade6c
Anish oli <olianish534@gmail.com>
2025-08-02 12:02:20 +0545

    Fix total items and total price

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .models import Customer, Order
+import traceback
+
 
 
 
@@ -19,10 +22,77 @@ def store(request):
 #view for cart
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create()
+        items = order.orderitem_set.all()
+    else:
+        items= []
+        order = {'get_cart_total':0,'get_cart_items':0}
+
+    context = {'items':items, 'order':order}
     return render(request, 'store/Cart.html' ,context)
 
+# def cart(request):
+#     try:
+#         if request.user.is_authenticated:
+#             try:
+#                 customer = request.user.customer
+#             except Customer.DoesNotExist:
+#                 customer = Customer.objects.create(user=request.user, name=request.user.username)
+
+#             order = Order.objects.filter(customer=customer, complete=False).order_by('-date_ordered').first()
+#             if not order:
+#                 order = Order.objects.create(customer=customer, complete=False)
+
+#             items = order.orderitem_set.all()
+#         else:
+#             items = []
+#             order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+#         context = {'items': items, 'order': order}
+#         return render(request, 'store/Cart.html', context)
+
+#     except Exception as e:
+#         print("🛑 Traceback:\n", traceback.format_exc())
+#         return HttpResponse(f"Error: {e}")
+    
 #view for checkout
+
 def checkout(request):
-    context = {}
-    return render(request, 'store/Checkout.html' ,context)
+    try:
+        if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create()
+            items = order.orderitem_set.all()
+        else:
+            items= []
+            
+            order = {'get_cart_total':0,'get_cart_items':0}
+            print(order)
+            
+        context = {'items':items, 'order':order}
+        return render(request, 'store/Checkout.html' ,context)
+
+    except Exception as e:
+        print("🛑 Traceback:\n", traceback.format_exc())
+        return HttpResponse(f"Error: {e}")
+
+# def checkout(request):
+#     if request.user.is_authenticated:
+#         try:
+#             customer = request.user.customer
+#         except Customer.DoesNotExist:
+#             customer = Customer.objects.create(user=request.user, name=request.user.username)
+
+#         order = Order.objects.filter(customer=customer, complete=False).order_by('-date_ordered').first()
+#         if not order:
+#             order = Order.objects.create(customer=customer, complete=False)
+
+#         items = order.orderitem_set.all()
+#     else:
+#         items = []
+#         order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+#     context = {'items': items, 'order': order}
+#     return render(request, 'store/Checkout.html', context)
